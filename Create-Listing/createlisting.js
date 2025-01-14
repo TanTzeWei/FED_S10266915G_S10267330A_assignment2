e88ae6759fe98da441dd33c83418698eef43f72f
DanishesCode <ezogetsthis@gmail.com>
2025-01-14 14:28:43 +0800

    did some things for create listing

---
+++ Create-Listing/createlisting.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded",function(){
+    const photo = document.querySelector("#photo");
+    //Line for os//
+    const condition = document.querySelector("");
+    const name = document.querySelector("#name");
+    const set = document.querySelector("");
+    const warranty = document.querySelector("");
+    const description = document.querySelector("description");
+    const meetup = document.querySelector("");
+    const address = document.querySelector("#address")
+    const delivery = document.querySelector("");
+})
