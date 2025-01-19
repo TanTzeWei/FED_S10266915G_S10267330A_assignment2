387099abb33aa7f001ff79e467db6fb1134f4f23
DanishesCode <ezogetsthis@gmail.com>
2025-01-19 18:40:14 +0800

    did the 3 dots for options

--- index.js
+++ index.js
@@ -60,11 +60,35 @@ function loadForYou(data){
         postTime.textContent = dateDiff+" days ago";
         name.textContent = selected.listingname;
         clone.style.visibility = "visible";
+        
+        clone.setAttribute("productId",selected.listingid);
+        clone.setAttribute("ownerId",selected.ownerid)
         forYou.appendChild(clone);
     }
 }
 
+function clickOption(e){
+  let target = e.target;
+  let productCard = target.parentElement.parentElement;
+  if(window.getComputedStyle(productCard.querySelector("#menu-example")).display == "none"){
+    if(productCard.getAttribute("ownerId") == localStorage.getItem("id")){
+      productCard.querySelector("#menu-example").style.display = "block";
+      productCard.querySelector("#menu-example #edit").style.display = "block";
+      productCard.querySelector("#menu-example #delete").style.display = "block";
+    }else{
+      productCard.querySelector("#menu-example").style.display = "block";
+      productCard.querySelector("#menu-example #report").style.display = "block";
+    }
+  }else{
+    productCard.querySelector("#menu-example").style.display = "none";
+      productCard.querySelector("#menu-example #edit").style.display = "hidden";
+      productCard.querySelector("#menu-example #delete").style.display = "hidden";
+  }
+  
+}
+
 document.addEventListener("DOMContentLoaded",async function(){
+  localStorage.setItem("id",1)
     let data = (await fetchListingsData());
     const trending = document.querySelector("#trendingList");
     const latest = document.querySelector("#latestList");
