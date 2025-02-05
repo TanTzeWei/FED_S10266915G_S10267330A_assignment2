5acae9d62bd34f9f361abf59293ba66924fe4d1f
DanishesCode <ezogetsthis@gmail.com>
2025-02-05 14:41:27 +0800

    halfway with edit profile

--- Edit-Profile/script.js
+++ Edit-Profile/script.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded",async function(){
-    
+
 })
+document.getElementById('fileInput').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            document.getElementById('profilePic').src = e.target.result;
+        };
+        reader.readAsDataURL(file);
+    }
+});
