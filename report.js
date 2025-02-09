18ec82367812f25ddaf8c14a7e54fd0bf923426a
TanTzeWei <S10266915@connect.np.edu.sg>
2025-02-09 12:55:20 +0800

    Fully Complete Report Page

--- report.js
+++ report.js
@@ -46,6 +46,22 @@ document.addEventListener("DOMContentLoaded",async function(){
         }
     });
   });
+  const imageInput = document.getElementById('screenshot');
+  const imagePreview = document.getElementById('image-preview');
+
+  imageInput.addEventListener('change', function() {
+      const file = this.files[0];
+      if (file) {
+          const reader = new FileReader();
+          reader.onload = function(e) {
+              imagePreview.src = e.target.result;
+              imagePreview.style.display = 'block';
+          };
+          reader.readAsDataURL(file);
+      } else {
+          imagePreview.style.display = 'none';
+      }
+  });
     let name = document.querySelector("#name");
     let email = document.querySelector("#email");
     let itemid = document.querySelector("#id");
